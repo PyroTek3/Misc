7b2b4f98559d2223b60cdbf3b3862e0cd8e157fe
PyroTek3 <PyroTek3@users.noreply.github.com>
2025-08-22 10:46:16 -0700

    Add files via upload

---
+++ Get-ADAdmins.ps1
@@ -0,0 +1,18 @@
+﻿# Note this script is designed for a single domain environment
+
+[array]$ADAdminArray = Get-ADGroupMember -Identity 'Administrators' -Recursive 
+
+$ADAdminPropertyArray = @()
+ForEach($ADAdminArrayItem in $ADAdminArray)
+ { 
+   SWITCH ($ADAdminArrayItem.objectClass)
+    {
+        'User' { [array]$ADAdminPropertyArray += Get-ADUser $ADAdminArrayItem.DistinguishedName -Prop * }
+        'Computer' { [array]$ADAdminPropertyArray += Get-ADComputer $ADAdminArrayItem.DistinguishedName -Prop *  }
+        'msDS-GroupManagedServiceAccount' { [array]$ADAdminPropertyArray += Get-ADServiceAccount $ADAdminArrayItem.DistinguishedName -Prop *  }
+    }
+ }
+
+$ADAdminPropertyArray | Sort SamAccountName | Select SamAccountName,ObjectClass,PasswordLastSet,LastLogonDate,Enabled,DoesNotRequirePreAuth,UseDESKeyOnly,PasswordNeverExpires,info,description | Format-Table -AutoSize
+
+
