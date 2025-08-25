b4dd35d3faf1668f559efa52d09dea4b3370b4e3
PyroTek3 <PyroTek3@users.noreply.github.com>
2025-08-25 09:28:33 -0700

    Add files via upload

---
+++ Get-BuiltInAdmins.ps1
@@ -0,0 +1,43 @@
+﻿# PowerShell script authored by Sean Metcalf (@PyroTek3)
+# 2025-08-22
+# Script provided as-is
+
+$DomainDC = 'TRD-ROOT-DC01'
+
+$GroupArray = @('Account Operators','Backup Operators','Cert Publishers','DNSAdmins','Enterprise Key Admins','Event Log Readers','IIS_IUSRS',`
+ 'Pre-Windows 2000 Compatible Access','Print Operators','Server Operators','Schema Admins')
+
+$PrivilegedGroupMemberArray = @()
+ForEach ($GroupArrayItem in $GroupArray)
+ {
+    $ADGroupMemberArray = Get-ADGroupMember $GroupArrayItem -Server $DomainDC -Recursive
+    $ADGroupMemberString = $ADGroupMemberArray.name -join ","
+    $PrivilegedGroupMemberItem = New-Object PSObject
+    $PrivilegedGroupMemberItem | Add-Member -MemberType NoteProperty -Name GroupName -Value $GroupArrayItem -Force
+    $PrivilegedGroupMemberItem | Add-Member -MemberType NoteProperty -Name MemberCount -Value $ADGroupMemberArray.Count -Force 
+    $PrivilegedGroupMemberItem | Add-Member -MemberType NoteProperty -Name Members -Value $ADGroupMemberString -Force 
+    $PrivilegedGroupMemberItem | Add-Member -MemberType NoteProperty -Name MemberArray -Value $ADGroupMemberArray -Force 
+    [array]$PrivilegedGroupMemberArray += $PrivilegedGroupMemberItem
+
+    IF ($ADGroupMemberArray.Count -gt 0)
+     { 
+       SWITCH ($GroupArrayItem)
+        {
+            'Account Operators' { Write-Warning "The $GroupArrayItem group should be empty, but contains $($ADGroupMemberArray.Count) members" }
+            'Print Operators' { Write-Warning "The $GroupArrayItem group should be empty, but contains $($ADGroupMemberArray.Count) members" }
+            'Schema Admins' { Write-Warning "The $GroupArrayItem group should be empty, but contains $($ADGroupMemberArray.Count) members" }
+        }
+     }
+
+    IF ( ($GroupArrayItem -eq 'Backup Operators') -AND ($ADGroupMemberArray.Count -ge 5) )
+     { Write-Warning "The $GroupArrayItem group should typically have less than ~5 members, but contains $($ADGroupMemberArray.Count) members" }
+    IF ( ($GroupArrayItem -eq 'Cert Publishers') -AND ($ADGroupMemberArray.Count -ge 5) )
+     { Write-Warning "The $GroupArrayItem group should typically have less than ~5 members, but contains $($ADGroupMemberArray.Count) members" }
+    IF ( ($GroupArrayItem -eq 'Event Log Readers') -AND ($ADGroupMemberArray.Count -ge 2) )
+     { Write-Warning "The $GroupArrayItem group should typically have less than ~3 members, but contains $($ADGroupMemberArray.Count) members" }
+    IF ( ($GroupArrayItem -eq 'IIS_IUSRS') -AND ($ADGroupMemberArray.Count -ge 2) )
+     { Write-Warning "The $GroupArrayItem group should typically have ~1 member, but contains $($ADGroupMemberArray.Count) members" }
+ }
+
+$PrivilegedGroupMemberArray | Select GroupName,MemberCount,Members | Format-Table -Auto
+
