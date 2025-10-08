04fe593e279ec00b0c1fd03b37f927f1771d20bc
PyroTek3 <PyroTek3@users.noreply.github.com>
2025-10-08 16:24:58 -0400

    Add files via upload

--- Invoke-FindPrivilegedFSPs.ps1
+++ Invoke-FindPrivilegedFSPs.ps1
@@ -11,12 +11,18 @@ $DomainDC = (Get-ADDomainController -Discover -DomainName $Domain).Name
 $DomainInfo = Get-ADDomain -Server $DomainDC
 $DomainEntry = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$($DomainInfo.DistinguishedName)")
 
-$GroupArray = @('Administrators','Account Operators','Backup Operators','Cert Publishers','DNSAdmins','Domain Admins','Enterprise Admins','Print Operators','Remote Desktop Users','Server Operators')
+#$GroupArray = @('Administrators','Account Operators','Backup Operators','Cert Publishers','DNSAdmins','Domain Admins','Enterprise Admins','Print Operators','Remote Desktop Users','Server Operators')
+$GroupArray = @('S-1-5-32-544','S-1-5-32-548','S-1-5-32-551',"-517",'-1101','-512','-519','S-1-5-32-550','S-1-5-32-555','S-1-5-32-549')
 
 ForEach ($GroupArrayItem in $GroupArray)
  {
-    Write-Host "Scanning $GroupArrayItem for FSPs..."
-    $GroupInfoDN = (Get-ADGroup -Identity $GroupArrayItem -Server $DomainDC).DistinguishedName
+    IF ($GroupArrayItem -like 'S-1-*')
+     { $GroupArrayItemSID = $GroupArrayItem }    
+    ELSE
+     { $GroupArrayItemSID = $($DomainInfo.DomainSID.Value) + $GroupArrayItem }
+    $GroupInfo = (Get-ADGroup -Identity $GroupArrayItemSID -Server $DomainDC)
+    $GroupInfoDN = $GroupInfo.DistinguishedName
+    Write-Host "Scanning $($GroupInfo.Name) for FSPs..."
     $GroupEntry = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$GroupInfoDN")
     $MemberArray = @()
     [array]$MemberArray = $groupEntry.Properties["member"]
@@ -25,7 +31,7 @@ ForEach ($GroupArrayItem in $GroupArray)
     IF ($FSPMemberArray)
      { 
          Write-Host ""
-         Write-Host "$GroupArrayItem FSP Members:" 
+         Write-Host "$($GroupInfo.Name) FSP Members:" 
         $FSPMemberArray
          Write-Host ""
      }  
